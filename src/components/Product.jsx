0611c73a359bf76bb11240c6eacf31de7f679846
mespinozar <rieraes@gmail.com>
2022-06-22 23:56:00 -0500

    Actualizado

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+
 
 
 
@@ -11,7 +11,7 @@ function Product({id, title, image}) {
         <h4 className="card-title">{title}</h4>
         <h5 className="card-title">Codigo: {id}</h5>
         <p className="card-text text-secondary">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, doloribus!</p>
-        <link to="/" className="btn btn-outline-secondary" target="_blank">Go to the Store</link>
+        <a href="#" className="btn btn-outline-secondary" target="_blank">Go to the Store</a>
       </div>
     </div>
   )
